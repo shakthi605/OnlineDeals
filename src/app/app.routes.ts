41ed112c8fae4e1b2132af1fac427ccf525b60a2
shiva <sivasakthi.s@tthconsulting.com>
2024-02-08 12:50:49 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './modules/customer/landing-page/landing-page.component';
+import { AllProductsComponent } from './modules/customer/all-products/all-products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'Home',
+        pathMatch: 'full'
+    },
+    {
+        path: '',
+        component: LandingPageComponent,
+        children: [
+            {
+                path: 'Home',
+                component: AllProductsComponent,
+                title: 'Online Deals'
+            }
+        ]
+    },
+];
